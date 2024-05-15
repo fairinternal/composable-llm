a5a145d25482b30d9eef1890711f31a28deb3492
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-05-15 09:58:31 +0000

    cosmetic changes

--- scripts/slurm_launcher.sh
+++ scripts/slurm_launcher.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/bash
 
 # Logging configuration
-#SBATCH --job-name=cot-copy
-#SBATCH --output=/checkpoint/vivc/cot-copy/%a-%t.out
-#SBATCH --error=/checkpoint/vivc/cot-copy/%a-%t.err
+#SBATCH --job-name=cot-binary
+#SBATCH --output=/checkpoint/%u/cot-binary/%a-%t.out
+#SBATCH --error=/checkpoint/%u/cot-binary/%a-%t.err
 #SBATCH --mail-type=END
-#SBATCH --mail-user=vivc@meta.com
+#SBATCH --mail-user=%u@meta.com
 
 # Job specification
-#SBATCH --partition=devlab
+#SBATCH --partition=scavenge
 #SBATCH --time=5:00:00
 #SBATCH --mem=256G
 #SBATCH --nodes=1
-#SBATCH --partition=scavenge
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-node=1
-#SBATCH --array=1-30
+#SBATCH --array=1-100
 
 
-python /private/home/vivc/code/llm/cot/scripts/grid_run.py --num-tasks $SLURM_ARRAY_TASK_COUNT --task-id $SLURM_ARRAY_TASK_ID --config_filename binary
+python /private/home/vivc/code/llm/cot/scripts/grid_run_exp1.py --num-tasks $SLURM_ARRAY_TASK_COUNT --task-id $SLURM_ARRAY_TASK_ID --config_filename exp1
